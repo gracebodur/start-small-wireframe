ecfb870c440cf709fae40c961edffce834a35071
Grace <mgracebodur@gmail.com>
2020-02-10 21:01:40 -0500

    Completed User page, added modals and links

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ![User flow](https://www.lucidchart.com/publicSegments/view/1d0b2ce2-581f-425e-8f5e-f25e505a23b0/image.png)
 
 # Screen inventory
-![Screen inventory](https://www.lucidchart.com/publicSegments/view/18c4772e-766d-4679-bf83-1773bced7847/image.png)
+![Screen inventory](https://www.lucidchart.com/publicSegments/view/a3691bf8-2d72-47cc-9590-f4824bcbeb8a/image.png)
 
 # Wireframe
 [Wireframe](https://gracebodur.github.io/start-small-wireframe/)
--- index.js
+++ index.js
@@ -1,13 +1,41 @@
-const attempt = 3;
+function registerSubmit() {
+  const fullName = document.getElementById('register_fullname').value
+  const registerUsername = document.getElementById('register_username').value
+  const registerPassword = document.getElementById('register_password').value
 
-function loginSubmit(form) {
-
- if(form.username.value == "grace" && form.password.value == "123")
-  {
+  if (!fullName) {
+    alert("Full name is required")
+    return false;
+  }
+  if (!registerUsername) {
+    alert("Username is required")
+    return false;
+  }
+  if (!registerPassword) {
+    alert("Password is required")
+    return false;
+  }
+  else {
     window.open('./users-page.html')
+    return true
+  }
+}
+
+function loginSubmit() {
+  const username = document.getElementById('username').value
+  const password = document.getElementById('login_password').value
+
+ if(username == "grace" && password == "123") {
+    window.open('./users-page.html')
+    return false
   }
  else
  {
-   alert("Error Password or Username")
+   alert('Username or password is incorrect')
+   return false
   }
+}
+
+function usersLogout() {
+  window.location.href="./index.html";
 }
